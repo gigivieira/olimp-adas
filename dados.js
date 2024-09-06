8672f88f0722964d1357cd490027b724d5f6706d
Giovanna Vieira <164201299+gigivieira@users.noreply.github.com>
2024-09-06 09:12:07 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal", 
+        descricao : "Rayssa Leal é uma skatista brasileira, vice-campeã olímpica nos Jogos Olímpicos de Verão de 2020 em Tóquio, sendo a mais jovem medalhista olímpica brasileira.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+        {
+          titulo: "Beatriz Souza",
+          descricao: "Beatriz Souza é uma judoca brasileira, campeã olímpica nos Jogos Olímpicos de Paris 2024, conquistando a primeira medalha de ouro do Brasil na categoria +78kg.",
+          link: "https://pt.wikipedia.org/wiki/Beatriz_Souza", 
+          tags: "judô judo judoca judoka"
+        },
+        {
+          titulo: "Rebeca Andrade",
+          descricao: "Rebeca Andrade é uma ginasta brasileira, campeã olímpica nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro no individual geral.",
+          link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+          tags: "ginastica ginástica ouro"
+        }
+      ];
